eb1740e6603eb64cb8fffc4ede67178740f48bbc
Punam807 <ppunamrani7@gmail.com>
2026-02-12 23:56:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("") || "";
+}
+showNotes();
+
+function updateStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+createBtn.addEventListener("click", () => {
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "images/images.png";
+
+  inputBox.appendChild(img);
+  notesContainer.appendChild(inputBox);
+});
+
+notesContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+    updateStorage();
+  } else if (e.target.tagName === "P") {
+    let notes = document.querySelectorAll(".input-box");
+    notes.forEach(nt => {
+      nt.onkeyup = function () {
+        updateStorage();
+      };
+    });
+  }
+});
